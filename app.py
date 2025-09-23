e4000994d5e8e41b98861ffef06c59fb389edc08
Павел Белков <disbalance77@yandex.ru>
2025-09-23 17:51:21 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# app.py
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # Потенциальная уязвимость: кривое преобразование данных от пользователя.
+    try:
+        user_id = int(request.args.get('id', 1))
+    except ValueError:
+        user_id = 1
+    return f'<h1>Hello, user #{user_id}!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Уязвимость! Никогда не используйте debug=True в продакшене.
