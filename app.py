8702101940a9acceca9408d21026279bb4cac0fe
Павел Белков <disbalance77@yandex.ru>
2025-10-15 01:35:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-# app.py
 from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    
     try:
         user_id = int(request.args.get('id', 1))
     except ValueError:
         user_id = 1
     return f'<h1>Hello, user #{user_id}!</h1>'
 
+
 if __name__ == '__main__':
-    app.run(debug=True)  
+    app.run(debug=True)
